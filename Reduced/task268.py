7f30286705eae08267904e89abc6c5ad3e9f8365
Frederick Lee <frederick.v.l@gmail.com>
2025-09-27 15:52:19 -0400

    save 5 bytes post-compression

--- Reduced/task268.py
+++ Reduced/task268.py
@@ -1,17 +1,16 @@
-def p(g,Q=range):
+def p(g):
  n=len(g);r=lambda g:[*map(list,zip(*g[::-1]))]
- for k in Q(4):
-  P=[(i,j)for i in Q(n)for j in Q(n)if g[i][j]]
+ for k in range(4):
+  P=[(i,j)for i in range(n)for j in range(n)if g[i][j]]
   I,J=zip(*P);U,D,L,R=min(I),max(I),min(J),max(J);c=g[U][L]
   u=g[U].count(c)
   if u<g[D].count(c):
    E,F=L+u//2,R-u//2
-   for i in Q(D):g[i][E:F+1]=[4]*(F-E+1)
-   for i in Q(U+1,D):g[i][L+1:R]=[4]*(R-L-1)
-   for j in Q(U+1):
-    i=U-j
-    if E>=j:g[i][E-j]=4
-    if F+j<n:g[i][F+j]=4
-   for _ in Q(4-k):g=r(g)
+   for i in range(D):g[i][E:F+1]=[4]*(F-E+1)
+   for i in range(U+1,D):g[i][L+1:R]=[4]*(R-L-1)
+   for j in range(U+1):
+    if E>=j:g[U-j][E-j]=4
+    if F+j<n:g[U-j][F+j]=4
+   for _ in range(4-k):g=r(g)
    return g
   g=r(g)
